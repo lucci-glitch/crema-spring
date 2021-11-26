8e687e9fbe83be947953ba4e7893bae04ad6d815
Henrik Büller <carl.henrik.buller@gmail.com>
2021-11-26 15:57:06 +0100

    Refactor testmain

--- src/main/java/com/crema/creamaspring/scraper/testmain.java
+++ src/main/java/com/crema/creamaspring/scraper/testmain.java
@@ -1,13 +1,19 @@
 package com.crema.creamaspring.scraper;
 
-import java.io.OutputStream;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class testmain {
-    public static void main(String[] args) {
-       /* TitleScraper test = new TitleScraper();
+    public static void main(String[] args) throws IOException, JSONException {
 
-        test.retrieveData();
-        System.out.println(test.retrieveData());*/
     }
 
 }
